102aefcf33b7fa5ff606cb5fd3af9198e754070e
U-SSC\ins_dcarver <ins_dcarver@LT-PK0A6X3.ssc.tsc>
2016-06-17 09:57:35 -0500

    futility

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 #dependecy_links = ["git+https://github.com/pexpect/pexpect.git#egg=pexpect-0.1"]
-install_requires = ['requests']
+#install_requires = ['requests']
 
 setup(
     name='satsh',
-    version='0.1',
+    version='0.2',
     packages=['satsh',],
-    install_requires=install_requires,
+    #install_requires=install_requires,
     entry_points = { 'console_scripts': [
         'satsh = satsh.satsh:cli', ],
      },
